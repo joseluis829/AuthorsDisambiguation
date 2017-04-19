cc0d056070883d6c15f4d32f05aa9048c17ed979
José Ortiz <jeortizvivar@gmail.com>
2017-04-19 12:32:05 -0500

    primera version

--- src/main/java/edu/ucuenca/authorsdisambiguation/AuthorsDisMain.java
+++ src/main/java/edu/ucuenca/authorsdisambiguation/AuthorsDisMain.java
@@ -5,10 +5,15 @@
  */
 package edu.ucuenca.authorsdisambiguation;
 
+import edu.ucuenca.authorsdisambiguation.nwd.CAD;
+import edu.ucuenca.authorsdisambiguation.nwd.Cache;
+import edu.ucuenca.authorsdisambiguation.nwd.NWD;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  *
@@ -26,6 +31,103 @@ public class AuthorsDisMain {
     //private static final KeywordGenerator keywordGenerator = new KeywordGenerator();
     
     public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException, Exception {
+        
+        //Cache.getInstance().prefix=UUID.randomUUID().toString()+"_";
+        
+        
+        //NWD d2 = new NWD();
+        
+        //double NWD = d2.NWD("http://190.15.141.66:8899/cedia/contribuyente/GONZALEZ__PATRICIA", "http://190.15.141.66:8890/myservice/sparql", "http://190.15.141.66:8899/cedia/contribuyente/GONZALEZ__PATRICIA_DRA", "http://190.15.141.66:8890/myservice/sparql", "prueba");
+        //String traductorYandex = d2.traductorYandex("telesalud");
+        //System.out.println(NWD);
+        
+        
+        //if (true)
+          // return;
+        
+        PrintWriter writer = new PrintWriter("/home/cedia/data.txt", "UTF-8");
+        
+        
+        
+        ModifiedJaccard me = new ModifiedJaccard();
+        me.Order=false;
+        System.out.println(me.Distance("Victor Hugo", "Hugo"));
+        me.Order=true;
+        System.out.println(me.Distance("Espinoza Mejía", "Espinosa Mejía"));
+        
+        //System.out.println(me.Sim("Josse","Josselin"));
+        //System.out.println(me.Sim("Mejía","Mejia"));
+
+        //if (true)
+            //return;
+        
+        
+        Distance d = new Distance();
+        
+        CAD disca = new CAD();
+        
+        NWD dic = new NWD();
+        
+        List<List<String>> c = d.c("", "http://190.15.141.102:8891/myservice/sparql");
+        
+        writer.println("Repositorio;URI;Nombre;Repositorio;URI;Nombre;SimilitudNombre;SimilitudApellido;Distancia;FactorCoautores;FactorAfiliacion;Total");
+        
+        
+        for (int i=0; i< c.size(); i++){
+            for (int j=i+1; j< c.size(); j++){
+            
+                List<String> get = c.get(i);
+                List<String> get1 = c.get(j);
+                
+                
+                if (get.get(0).compareTo(get1.get(0)) == 0){
+                    continue;
+                }
+                
+                me.Order=false;
+                double Distance = me.Distance(get.get(1), get1.get(1));
+                me.Order=true;
+                double Distance1 = me.Distance(get.get(2), get1.get(2));
+                
+                
+                double prom = (Distance+Distance1)/2;
+                
+                
+                if (Distance > 0.5 && Distance1 > 0.5){
+                    
+                    String ep1 =get.get(3).compareTo("UDC") ==0 ? "http://190.15.141.102:8891/myservice/sparql" : "http://190.15.141.66:8890/myservice/sparql";
+                    String ep2 =get1.get(3).compareTo("UDC") ==0 ? "http://190.15.141.102:8891/myservice/sparql" : "http://190.15.141.66:8890/myservice/sparql";
+                    
+                    double AF =1;
+                    if (get.get(3).compareTo(get1.get(3))==0){
+                        AF=0.9;
+                    }
+                    
+                    double NWD = dic.NWD(get.get(0), ep1, get1.get(0), ep2, "");
+                    double CAD = disca.CAD(get.get(0), ep1, get1.get(0), ep2);
+                    
+                    
+                    double DisTot=NWD*CAD*AF;
+                    
+                    
+                    String ll= get.get(3)+";"+get.get(0)+";"+get.get(1)+" "+get.get(2)+";"+get1.get(3)+";"+get1.get(0)+";"+get1.get(1)+" "+get1.get(2)+";"+Distance+";"+Distance1+";"+NWD+";"+CAD+";"+AF+";"+DisTot;
+                    writer.println(ll);
+                    writer.flush();
+                    System.out.println(ll);
+                    
+                    
+                    
+                    System.out.println(""+NWD);
+                }
+                
+            }
+        }
+        
+        writer.close();
+        
+        if(true)
+        return ;
+        
 
         List<String> lista1 = new ArrayList<>();//keywordGenerator.getKeywords("Facultad de medicina, Investigación científica, Universidad de Guayaquil","");
         lista1.add("medicine");
